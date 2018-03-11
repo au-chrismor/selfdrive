71a82167c15e6c30f49d380d34bd4580bb28d4bf
Christopher Moran <chris.moran@emergent.com.au>
2018-03-11 22:26:51 +1100

    Added skeleton for serial comms using WiringPi

--- src/experiments/videostream/videostream.cpp
+++ src/experiments/videostream/videostream.cpp
@@ -8,22 +8,36 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <stdio.h>
+#include <errno.h>
+#ifdef _HAS_SERIAL
+#include <wiringSerial.h>
+#endif
 
 using namespace cv;
 using namespace std;
 
 int main(int, char**)
 {
+	int com = 0;
+
+#ifdef _HAS_SERIAL
+	com = serialOpen("/dev/serial1", 115200);
+	if(com == -1)
+	{
+		printf("Error %d opening serial line\n", errno);
+		return -1;
+	}
+#endif
     Mat frame, frame1;
     //--- INITIALIZE VIDEOCAPTURE
     VideoCapture cap;
     // open the default camera using default API
-    //cap.open(0);
+    cap.open(0);
     // OR advance usage: select any API backend
     int deviceID = 0;             // 0 = open default camera
     int apiID = cv::CAP_ANY;      // 0 = autodetect default API
     // open selected camera using selected API
-    cap.open(deviceID + apiID);
+    //cap.open(deviceID + apiID);
     // check if we succeeded
     if (!cap.isOpened())
     {
@@ -35,6 +49,8 @@ int main(int, char**)
     int frame_height = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 
     //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+        << "Press any key to terminate" << endl;
     for (;;)
     {
         // wait for a new frame from camera and store it into 'frame'
@@ -84,20 +100,18 @@ int main(int, char**)
         float yRight = mLeft.m01 / mLeft.m00;
 
         float w2 = frame.cols / 2.0f;
-        // k gives us an "off-centre" value for our path
-        // When k = 0, we need to set the steering servo to 90 degrees.
-        // which corresponds to straight ahead.
+	float h2 = frame.rows / 2.0f;
         float k = (w2 - x) / w2 * 2;
-/*
-        int speed = 35;
-        int vr = speed;
-        int vl = speed;
+//        int speed = 35;
+//        int vr = speed;
+//        int vl = speed;
 
-        if (k >= 0)
-            vl = (1 - fabs(k)) * speed;
-        else
-            vr = (1 - fabs(k)) * speed;
-*/
+//        if (k >= 0)
+//            vl = (1 - fabs(k)) * speed;
+//        else
+//            vr = (1 - fabs(k)) * speed;
+
+	printf("w2=%4.1f h2=%4.1f x=%f k=%f\n", w2, h2, x, k);
 #ifdef _GUI
         rectangle(frame, grid[0], Scalar( 255, 0, 0), 2, 8); //bottom
         rectangle(frame, grid[1], Scalar( 255, 0, 0), 2, 8); //top
@@ -109,7 +123,18 @@ int main(int, char**)
         circle(frame, Point(x + 100, frame.rows - 150 - y), 10, Scalar( 255, 0, 0), 3, 8); //bottom
         circle(frame, Point(x2 + 160, frame.rows - 250 - y2), 10, Scalar( 0, 255, 0), 3, 8); //top
 #endif
-        printf("w2=%f x=%f k=%f\n", w2, x, k);
+
+#ifdef _HAS_SERIAL
+	// Send the command to our steering controller
+	if (k > 0)
+	{
+		serialPutchar(com, '.');
+	}
+	if (k < 0)
+	{
+		serialPutchar(com, ',');
+	}
+#endif
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
