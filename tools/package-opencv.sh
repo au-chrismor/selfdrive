09374f5a8c38f5d591b458213d39f75b740592aa
Christopher Moran <chris.moran@emergent.com.au>
2018-03-15 15:53:03 +1100

    Add architecture support

--- tools/package-opencv.sh
+++ tools/package-opencv.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -xe
 #
 cd /usr/local
-tar -cvzf /tmp/opencv-dist-$(ARCH).tar.gz bin/opencv* lib/libopencv* lib/python3.5/* lib/python2.7* include/opencv include/opencv2 share/OpenCV
+tar -cvzf /tmp/opencv-dist-$ARCH.tar.gz bin/opencv* lib/libopencv* lib/python3.5/* lib/python2.7* include/opencv include/opencv2 share/OpenCV
 #
-mv /tmp/opencv-dist-$(ARCH).tar.gz $(DISTDIR)
+mv /tmp/opencv-dist-$ARCH.tar.gz $DISTDIR
 
