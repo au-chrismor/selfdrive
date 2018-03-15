b6ea80345a5cf3f4863774d8f84bd08ce450e13e
Christopher Moran <chris.moran@emergent.com.au>
2018-03-15 15:46:50 +1100

    Adding packaging for opencv

---
+++ tools/package-opencv.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -xe
+#
+cd /usr/local
+tar -cvzf /tmp/opencv-dist-$(ARCH).tar.gz bin/opencv* lib/libopencv* lib/python3.5/* lib/python2.7* include/opencv include/opencv2 share/OpenCV
+#
+mv /tmp/opencv-dist-$(ARCH).tar.gz $(DISTDIR)
+
