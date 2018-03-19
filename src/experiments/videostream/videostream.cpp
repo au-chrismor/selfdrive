b3148dfa5adbc89edbd31cdb9122faa866e935ea
Christopher Moran <chris.moran@emergent.com.au>
2018-03-19 12:43:59 +1100

    Adding motion controls to videostream

--- src/experiments/videostream/videostream.cpp
+++ src/experiments/videostream/videostream.cpp
@@ -14,7 +14,7 @@ int main(int, char**)
 	int com = 0;
 
 #ifdef _HAS_SERIAL
-	com = serialOpen("/dev/serial1", 115200);
+	com = serialOpen("/dev/serial1", 115200);               // This name is WRONG for an RPi...
 	if(com == -1)
 	{
 		printf("Error %d opening serial line\n", errno);
@@ -123,10 +123,14 @@ int main(int, char**)
 	{
 		serialPutchar(com, '.');
 	}
-	if (k < 0)
+        else if (k < 0)
 	{
 		serialPutchar(com, ',');
 	}
+        else
+        {
+                serialPutChar(com, 'f');        // Move
+        }
 #endif
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
