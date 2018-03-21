06b55e8c476ccb87d1e8cdd3da1547806ea231d8
Christopher Moran <chris.moran@emergent.com.au>
2018-03-21 14:39:35 +1100

    Added LCD Dashboard

--- src/arduino/steering/steering-hw.h
+++ src/arduino/steering/steering-hw.h
@@ -1,11 +1,14 @@
-#define MAX_ADDR    0
-#define MIN_ADDR    1
+#define MAX_ADDR        0
+#define MIN_ADDR        1
 
 
-#define MOTOR_A     2
-#define MOTOR_B     4 
+#define MOTOR_A         2
+#define MOTOR_B         4 
+#define US_OUT          5       // Ultrasonic sensor trig to RPi
+#define SERVO_PIN       9
+#define TRIG_PIN        10
+#define ECHO_PIN        12
+#define LED_PIN         13
 
-#define SERVO_PIN   9
-#define TRIG_PIN    10
-#define ECHO_PIN    12
-#define LED_PIN     13
+#define I2C_ADDR        0x27
+#define IBATT           (A0)
