d33e3d15945dcc57233c61aba1b84f0e2d1818bc
Chris Moran <au-chrismor@users.noreply.github.com>
2023-08-14 13:25:27 +1000

    Clean up after sonar scan

--- src/python/coordcalc.py
+++ src/python/coordcalc.py
@@ -5,7 +5,7 @@ import math
 # Radius of the Earth is approximately 6,371Km
 radius = 6371000
 
-def calcDistance(startLat, startLong, endLat, endLong):
+def calc_distance(startLat, startLong, endLat, endLong):
     startPhi = math.radians(startLat)
     endPhi = math.radians(endLat)
     deltaLat = math.radians((endLat - startLat))
@@ -16,7 +16,7 @@ def calcDistance(startLat, startLong, endLat, endLong):
     dist = radius * c
     return dist
 
-def calcBearing(startLat, startLong, endLat, endLong):
+def calc_bearing(startLat, startLong, endLat, endLong):
     y = math.sin(math.radians(endLong)-math.radians(startLong)) * math.cos(math.radians(endLat))
     x = math.cos(math.radians(startLat))*math.sin(math.radians(endLat)) - math.sin(math.radians(startLat))*math.cos(math.radians(endLat))*math.cos(math.radians(endLong)-math.radians(startLong))
     bearing = math.degrees(math.atan2(y, x))
@@ -24,7 +24,7 @@ def calcBearing(startLat, startLong, endLat, endLong):
         bearing = 360 + bearing
     return bearing
 
-def calcMidPoint(startLat, startLong, endLat, endLong):
+def calc_midpoint(startLat, startLong, endLat, endLong):
     ret = []
     bX = math.cos(math.radians(endLat)) * math.cos(math.radians(endLong)-math.radians(startLong))
     bY = math.cos(math.radians(endLat)) * math.sin(math.radians(endLong)-math.radians(startLong))
@@ -34,7 +34,7 @@ def calcMidPoint(startLat, startLong, endLat, endLong):
     ret.append(midLong)
     return ret
 
-def calcDestination(startLat, startLong, bearing, distance):
+def calc_destination(startLat, startLong, bearing, distance):
     ret = []
     endLat = math.degrees(math.asin( math.sin(math.radians(startLat))*math.cos(distance/radius) + math.cos(math.radians(startLat))*math.sin(distance/radius)*math.cos(math.radians(bearing))))
     endLong = startLong + math.atan2(math.sin(math.radians(bearing))*math.sin(distance/radius)*math.cos(math.radians(startLat)), math.cos(distance/radius)-math.sin(math.radians(startLat))*math.sin(math.radians(endLat)))
@@ -42,8 +42,8 @@ def calcDestination(startLat, startLong, bearing, distance):
     ret.append(endLong)
     return ret
 
-print('Distance={0} metres'.format(calcDistance(-33.864937, 151.210059, -33.720171, 151.027125)))
-print('Bearing={0} degrees (mag)'.format(calcBearing(-33.864937, 151.210059, -33.720171, 151.027125)))
-print('Midpoint={0}'.format(calcMidPoint(-33.864937, 151.210059, -33.720171, 151.027125)))
-print('Destination={0}'.format(calcDestination(-33.864937, 151.210059, calcBearing(-33.864937, 151.210059, -33.720171, 151.027125), calcDistance(-33.864937, 151.210059, -33.720171, 151.027125))))
+print('Distance={0} metres'.format(calc_distance(-33.864937, 151.210059, -33.720171, 151.027125)))
+print('Bearing={0} degrees (mag)'.format(calc_bearing(-33.864937, 151.210059, -33.720171, 151.027125)))
+print('Midpoint={0}'.format(calc_midpoint(-33.864937, 151.210059, -33.720171, 151.027125)))
+print('Destination={0}'.format(calc_destination(-33.864937, 151.210059, calc_bearing(-33.864937, 151.210059, -33.720171, 151.027125), calc_distance(-33.864937, 151.210059, -33.720171, 151.027125))))
 
