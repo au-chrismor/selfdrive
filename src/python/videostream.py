714dbb0749ce81107bdfd950428e638f86f83ebe
Chris Moran <au-chrismor@users.noreply.github.com>
2023-08-14 11:30:00 +1000

    Clean-up unused code

--- src/python/videostream.py
+++ src/python/videostream.py
@@ -7,11 +7,9 @@ def processImage(image):
     print("width={}, height={}, depth={}".format(w, h, d))
     #roi = image[0:480, 0:1280]
     roi1 = image[0:480, 0:639]
-#    roi2 = image[0:480, 640:1279]
 
     grey = cv2.cvtColor(roi1, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Image 1", roi1)
-#    cv2.imshow("Image 2", roi2)
     cv2.imshow("Grey", grey)
     cv2.waitKey(0)
 
