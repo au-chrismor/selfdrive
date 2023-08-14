57fa8b56b6287982e919d4a2c4246c7cef7c9949
Chris Moran <au-chrismor@users.noreply.github.com>
2023-08-14 13:23:17 +1000

    Clean up after Sonar Review

--- src/python/compass.py
+++ src/python/compass.py
@@ -12,7 +12,7 @@ Z_axis_H = 0x05
 Y_axis_H = 0x07
 declination = -0.00669
 
-def MagnetometerInit():
+def Magnetometer_init():
 	# Write to the configuration register A
 	bus.write_byte_data(Device_Address, Register_A, 0x70)
 	# Configuration Register B gain
@@ -20,7 +20,7 @@ def MagnetometerInit():
 	# Select mode
 	bus.write_byte_data(Device_Address, Register_mode, 0)
 
-def ReadRawData(addr):
+def read_raw_data(addr):
 	# Read the raw 16-bit value
 	high = bus.read_byte_data(Device_Address, addr)
 	low = bus.read_byte_data(Device_Address, addr + 1)
@@ -35,14 +35,14 @@ def ReadRawData(addr):
 bus = smbus.SMBus(2)
 Device_Address = 0x1e
 
-MagnetometerInit()
+Magnetometer_init()
 
 print('Reading\tHeading\tAngle')
 while True:
 	# Read the raw data
-	x = ReadRawData(X_axis_H)
-	y = ReadRawData(Y_axis_H)
-	z = ReadRawData(Z_axis_H)
+	x = read_raw_data(X_axis_H)
+	y = read_raw_data(Y_axis_H)
+	z = read_raw_data(Z_axis_H)
 
 	heading = math.atan2(y, x) + declination
 	if heading > 2 * math.pi:
